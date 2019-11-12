562d558aad1fdaeeb6a1e65155281cec8bffbb94
andreabocchimpani <57347307+andreabocchimpani@users.noreply.github.com>
2019-11-12 17:02:46 +0100

    First Commit

---
+++ SportManager/LogicLayer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SportManager.Model;
+
+namespace SportManager
+{
+    class LogicLayer : ILogic
+    {
+
+        public IPersistence MyPersitence { get; set; }
+
+        public LogicLayer(IPersistence persistence)
+        {
+            //MyPersitence = new PersistenceLayer();
+            MyPersitence = persistence;
+        }
+        public Athlete[] getAllAthletes()
+        {
+            Athlete[] athleteArray = MyPersitence.AllAthlete; //AllAthlete esiste in PersistenceLayer
+            return athleteArray;
+        }
+
+        public void insertAthlete(Athlete athlete)
+        {
+            Console.WriteLine(athlete);
+        }
+
+        public Athlete searchAthlete(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
